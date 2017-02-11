898e9da17c061fb3ab2ada740c039686ac208a9c
Ariel Cazon <arielc@lagash.com>
2017-02-11 17:40:31 -0300

    busqueda y ordenamiento

--- PremptyWorkSpace/Controllers/EmpleadoController.cs
+++ PremptyWorkSpace/Controllers/EmpleadoController.cs
@@ -25,15 +25,55 @@ namespace PremptyWorkSpace.Controllers
         }
 
         //Devuelve todos los empleados por area que se asigno (como obtener ese dato?)
-        public ActionResult Details(int id)
+        public ViewResult Details(int id, string sortOrder, string searchString)
         {
+            ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            ViewBag.LastNameSortParm = sortOrder == "lastname" ? "lastname_desc" : "lastname";
+            ViewBag.UserNameSortParm = sortOrder == "username" ? "username_desc" : "username";
+            ViewBag.DateSortParm = sortOrder == "date" ? "date_desc" : "date";
+            ViewBag.Id = id;
 
-            var user = (from u in db.Usuarios
-                        where u.IdArea.Equals(id)
-                        select u).ToList();
 
+            var users = (from u in db.Usuarios
+                         where u.IdArea.Equals(id)
+                         select u);
 
-            return View(user);
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                users = users.Where(x => x.NombreUsuario.Contains(searchString)
+                                       || x.Apellido.Contains(searchString)
+                                       || x.Nombre.Contains(searchString));
+            }
+
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    users = users.OrderByDescending(s => s.Nombre);
+                    break;
+                case "date":
+                    users = users.OrderBy(s => s.FechaIngreso);
+                    break;
+                case "date_desc":
+                    users = users.OrderByDescending(s => s.FechaIngreso);
+                    break;
+                case "lastname":
+                    users = users.OrderBy(s => s.Apellido);
+                    break;
+                case "lastname_desc":
+                    users = users.OrderByDescending(x => x.Apellido);
+                    break;
+                case "username":
+                    users = users.OrderBy(x => x.IdUsuario);
+                    break;
+                case "username_desc":
+                    users = users.OrderByDescending(x => x.NombreUsuario);
+                    break;
+                default:
+                    users = users.OrderBy(s => s.Nombre);
+                    break;
+            }
+
+            return View(users.ToList());
         }
 
 
