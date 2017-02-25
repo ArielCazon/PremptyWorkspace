6177ecaca03c531c7d3f1c6c996980f18583f54e
Rodrigo Yucra <yucramitarodrigo@gmail.com>
2017-02-25 13:34:43 -0300

    Marge Rodrigo

---
+++ PremptyWorkSpace/Models/UsuarioListado.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PremptyWorkSpace.Models
+{
+    public class UsuarioListado
+    {
+        public int Legajo { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        [DisplayName("Descripcion de Area")]
+        public string DescrArea { get; set; }
+        [DisplayName("Cantidad de Inasistencia")]
+        public int CantInas { get; set; }
+    }
+}
